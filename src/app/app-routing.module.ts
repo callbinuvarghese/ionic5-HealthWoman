6e207892b0831d3cb3b0f62d5c3d627af76403ef
callbinuvarghese <call.binu.varghese@gmail.com>
2022-03-10 09:46:07 -0500

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'folder/Inbox',
+    pathMatch: 'full'
+  },
+  {
+    path: 'folder/:id',
+    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'cycle',
+    loadChildren: () => import('./pages/cycle/cycle.module').then( m => m.CyclePageModule)
+  },
+  {
+    path: 'ovulation',
+    loadChildren: () => import('./pages/ovulation/ovulation.module').then( m => m.OvulationPageModule)
+  },
+  {
+    path: 'pregnancy',
+    loadChildren: () => import('./pages/pregnancy/pregnancy.module').then( m => m.PregnancyPageModule)
+  },
+  {
+    path: 'infertility',
+    loadChildren: () => import('./pages/infertility/infertility.module').then( m => m.InfertilityPageModule)
+  },
+  {
+    path: 'safe-period',
+    loadChildren: () => import('./pages/safe-period/safe-period.module').then( m => m.SafePeriodPageModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./pages/contact/contact.module').then( m => m.ContactPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
+    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  constructor(private router: Router,  private platform: Platform,) {
+    this.initializeApp();
+    // Exit App on back button click from Home page
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      if (window.location.pathname === '/home') {
+        navigator['app'].exitApp();
+      }
+    });
+  }
+
+  initializeApp() {
+    // Set new Home page
+    this.router.navigateByUrl('home');
+  }
+}
+
